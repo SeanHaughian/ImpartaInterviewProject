ca8985c1ee5bb803acbd540fd0c92f96b2e9e213
Sean Haughian <seanhaughian@live.co.uk>
2023-11-05 22:15:28 +0000

    CORS + Get Front end

--- ImpartaInterviewProject/Startup.cs
+++ ImpartaInterviewProject/Startup.cs
@@ -52,6 +52,8 @@ namespace ImpartaInterviewProject
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
 		public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
 		{
+			app.UseCors(options => options.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
 			if (env.IsDevelopment())
 			{
 				app.UseDeveloperExceptionPage();
