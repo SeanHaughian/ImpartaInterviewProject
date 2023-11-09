707027efb4d77d1bb42c6d2896f2634baa671fc7
Sean Haughian <seanhaughian@live.co.uk>
2023-11-09 23:20:02 +0000

    logout redirect

--- ImpartaInterviewProject/imparta-app/src/Tasks.js
+++ ImpartaInterviewProject/imparta-app/src/Tasks.js
@@ -524,7 +524,7 @@ export class Tasks extends Component {
                 </div>
               </div>
             </div>
-          ) : null}
+          ) : this.logoutClick()}
         </div>
       </div>
     );
