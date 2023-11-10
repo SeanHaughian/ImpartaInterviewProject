369f0c9f9c4690b62c4f955b7db383d17be5dc85
Sean Haughian <seanhaughian@live.co.uk>
2023-11-10 00:33:31 +0000

    Sorting added

--- ImpartaInterviewProject/imparta-app/src/Tasks.js
+++ ImpartaInterviewProject/imparta-app/src/Tasks.js
@@ -47,6 +47,18 @@ export class Tasks extends Component {
 
 }
 
+sortResult(prop,asc){
+  var sortedData=this.state.tasksWithoutFilter.sort(function(a,b){
+      if(asc){
+          return (a[prop]>b[prop])?1:((a[prop]<b[prop])?-1:0);
+      }
+      else{
+          return (b[prop]>a[prop])?1:((b[prop]<a[prop])?-1:0);
+      }
+  });
+
+  this.setState({tasks:sortedData});
+}
 
   refreshList() {
     let tokenID = window.localStorage.getItem("token");
@@ -347,16 +359,45 @@ changeTasksNameFilter = (e)=>{
                 <thead>
                   <tr>
                     <th>
+                      <div className="d-flex flex-row">
                     <input className="form-control m-2"
-            onChange={this.changeTasksIdFilter}
-            placeholder="Filter"/>
+                      onChange={this.changeTasksIdFilter}
+                      placeholder="Filter"/>
+                      <button type="button" className="btn btn-light"
+                      onClick={()=>this.sortResult('id', true)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-down-square-fill" viewBox="0 0 16 16">
+                          <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5a.5.5 0 0 1 1 0z"/>
+                        </svg>
+                      </button>
+                      <button type="button" className="btn btn-light"
+                      onClick={()=>this.sortResult('id', false)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-square-fill" viewBox="0 0 16 16">
+                          <path d="M2 16a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2zm6.5-4.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 1 0z"/>
+                        </svg>
+                      </button>
+                      </div>
                       Task ID
                     </th>
                     <th>
+                      <div className="d-flex flex-row">
                     <input className="form-control m-2"
-            onChange={this.changeTasksNameFilter}
-                                            placeholder="Filter" />
-Name                                    </th>
+                      onChange={this.changeTasksNameFilter}
+                      placeholder="Filter"/>
+                      <button type="button" className="btn btn-light"
+                      onClick={()=>this.sortResult('name', true)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-arrow-down-square-fill" viewBox="0 0 16 16">
+                          <path d="M2 0a2 2 0 0 0-2 2v12a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V2a2 2 0 0 0-2-2H2zm6.5 4.5v5.793l2.146-2.147a.5.5 0 0 1 .708.708l-3 3a.5.5 0 0 1-.708 0l-3-3a.5.5 0 1 1 .708-.708L7.5 10.293V4.5a.5.5 0 0 1 1 0z"/>
+                        </svg>
+                      </button>
+                      <button type="button" className="btn btn-light"
+                      onClick={()=>this.sortResult('name', false)}>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-up-square-fill" viewBox="0 0 16 16">
+                          <path d="M2 16a2 2 0 0 1-2-2V2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2zm6.5-4.5V5.707l2.146 2.147a.5.5 0 0 0 .708-.708l-3-3a.5.5 0 0 0-.708 0l-3 3a.5.5 0 1 0 .708.708L7.5 5.707V11.5a.5.5 0 0 0 1 0z"/>
+                        </svg>
+                      </button>
+                      </div>
+                      Name
+                    </th>
                     <th>Status</th>
                     <th>Type</th>
                     <th>Priority</th>
